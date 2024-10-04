8595d6570e1700fb8a0c70db2f8c7520e7fb0df4
karolinaaa2 <hryhorchuk.karolina@chnu.edu.ua>
2024-10-05 00:57:27 +0300

    Add project files.

---
+++ Ex_8_loop.operator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введіть перше число: ");
+        int a = int.Parse(Console.ReadLine());
+        Console.Write("Введіть друге число: ");
+        int b = int.Parse(Console.ReadLine());
+
+        int gcd = GCD(a, b);
+        Console.WriteLine($"Найбільший спільний дільник для {a} та {b}: {gcd}");
+    }
+
+    static int GCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+}
